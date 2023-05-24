ad34c79b7dee1fdc8944e8f780ffc0d0705bd0b8
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-05-24 14:41:06 +0530

    Initial Commit

---
+++ src/main/java/com/example/firebasecrudspringboot/FirebaseCrudSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.firebasecrudspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseCrudSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirebaseCrudSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firebasecrudspringboot/FirebaseCrudSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firebasecrudspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseCrudSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
