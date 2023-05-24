44d13fd3b0c1d6e33a36b8040f49a95fe10f351e
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-05-24 16:08:25 +0530

    Part 2 Complete

---
+++ src/main/java/com/example/firebasecrudspringboot/CRUD.java
@@ -0,0 +1,14 @@
+package com.example.firebasecrudspringboot;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CRUD {
+
+    private String document_id;
+    private String name;
+    private String profession;
+
+}
--- src/main/java/com/example/firebasecrudspringboot/FirebaseCrudSpringBootApplication.java
+++ src/main/java/com/example/firebasecrudspringboot/FirebaseCrudSpringBootApplication.java
@@ -1,13 +1,38 @@
 package com.example.firebasecrudspringboot;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class FirebaseCrudSpringBootApplication {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
 
-    public static void main(String[] args) {
+@SpringBootApplication
+public class  FirebaseCrudSpringBootApplication {
+
+    public static void main(String[] args) throws IOException {
+
+        ClassLoader classLoader = FirebaseCrudSpringBootApplication.class.getClassLoader();
+
+        File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+
+        FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://fir-crud-springboot-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
         SpringApplication.run(FirebaseCrudSpringBootApplication.class, args);
+
+
+
     }
 
 }
